b084a17b8918730e7aa0a8cff91bc946fe53866f
Nishant <nishantbhosale244@gmail.com>
2021-07-27 19:15:44 +0530

    Function to draw tree using turtle

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello')
+from turtle import *
+
+shape('turtle')
+speed(1)
+
+def tree(size, levels, angle):
+   if levels == 0:
+      return
+   forward(size)
+   right(angle)
+
+   tree(size * 0.8, levels - 1, 30 )
+
+   left(angle * 2)
+   tree(size * 0.8, levels - 1, angle)
+
+   right(angle)
+   backward(size)
+
+left(90)
+tree(70, 5, 30)
+
+mainloop()
